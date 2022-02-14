d444e5e87efde30695968d5e1ba819528116a899
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2022-02-13 23:46:13 -0400

    Código definitivo em Pep8, sem warnings e typos
    O código foi reestruturado em alguns trechos para que seguisse efetivamente o padrão Pep8 e não mais compreendesse warnings e typos. Apesar disso, o programa ainda continua o mesmo da última atualização.

--- PongCode.py
+++ PongCode.py
@@ -3,32 +3,34 @@ import winsound
 import time
 
 # draw screen
-screen = turtle.Screen()
-screen.title("My Pong")
+from turtle import Screen, Turtle
+
+screen: Screen = turtle.Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
+screen.title("My Pong")
 screen.tracer(0)
+screen.setup(width=800, height=600)
 
 # draw paddle 1
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
+paddle_1: Turtle = turtle.Turtle()
 paddle_1.shape("square")
-paddle_1.color("white")
+paddle_1.speed(0)
 paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.color("white")
 paddle_1.penup()
 paddle_1.goto(-350, 0)
 
 # draw paddle 2
-paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
+paddle_2: Turtle = turtle.Turtle()
 paddle_2.shape("square")
-paddle_2.color("white")
+paddle_2.speed(0)
 paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.color("white")
 paddle_2.penup()
 paddle_2.goto(350, 0)
 
 # draw ball
-ball = turtle.Turtle()
+ball: Turtle = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
@@ -44,7 +46,7 @@ player_2 = screen.textinput("Player 2", "Name of the player: ")
 # score
 score_1 = 0
 score_2 = 0
-score_win =10
+score_win = 10
 
 # head-up display
 hud = turtle.Turtle()
@@ -63,7 +65,7 @@ hud.write(
 
 def paddle_1_up():
     y = paddle_1.ycor()
-    if y < 250:
+    if paddle_1.ycor() < 250:
         y += 30
     else:
         y = 250
@@ -81,7 +83,7 @@ def paddle_1_down():
 
 def paddle_2_up():
     y = paddle_2.ycor()
-    if y < 250:
+    if paddle_2.ycor() < 250:
         y += 30
     else:
         y = 250
@@ -90,7 +92,7 @@ def paddle_2_up():
 
 def paddle_2_down():
     y = paddle_2.ycor()
-    if y > -250:
+    if paddle_2.ycor() > -250:
         y += -30
     else:
         y = -250
@@ -105,14 +107,15 @@ screen.onkeypress(paddle_2_up, "Up")
 screen.onkeypress(paddle_2_down, "Down")
 
 while True:
-    screen.update()
 
-    # ball movement
+    # bal movement
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
+    screen.update()
+
     # collision with the upper wall
-    if ball.ycor() > 290:
+    if 290 < ball.ycor():
         winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
         ball.sety(290)
         ball.dy *= -1
@@ -141,8 +144,8 @@ while True:
             ball.dy = -0.3
     # collision with right wall
     if ball.xcor() > 390:
-        score_1 += 1
         hud.clear()
+        score_1 += 1
         hud.write(
             str(player_1) + f"  {score_1} " + f": {score_2}  " + str(player_2),
             align="center",
@@ -158,18 +161,18 @@ while True:
 
     # collision with the paddle 1
     if (
-        -340 < ball.xcor() < -330
-        and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50
+            -340 < ball.xcor() < -330
+            and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50
     ):
         ball.setx(-330)
-        # horizontal collision
+        # speed 1
         if paddle_1.ycor() + 10 > ball.ycor() > paddle_1.ycor() - 10:
             if ball.dy > 0:
                 ball.dy = 0.2
             if ball.dy < 0:
                 ball.dy = -0.2
             ball.dx *= -1
-        # collision fast
+        # speed 2
         if paddle_1.ycor() + 40 > ball.ycor() > paddle_1.ycor() + 10 \
                 or paddle_1.ycor() - 10 > ball.ycor() > paddle_1.ycor() - 40:
             if ball.dy > 0:
@@ -177,7 +180,7 @@ while True:
             if ball.dy < 0:
                 ball.dy = -0.4
             ball.dx = 0.4
-        # collision super fast
+        # speed 3
         if paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() + 40 \
                 or paddle_1.ycor() - 40 > ball.ycor() > paddle_1.ycor() - 50:
             if ball.dy > 0:
@@ -189,18 +192,18 @@ while True:
 
     # collision with the paddle 2
     if (
-        340 > ball.xcor() > 330
-        and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50
+            340 > ball.xcor() > 330
+            and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50
     ):
         ball.setx(330)
-        # horizontal collision
+        # speed 1
         if paddle_2.ycor() + 10 > ball.ycor() > paddle_2.ycor() - 10:
             if ball.dy > 0:
                 ball.dy = 0.2
             if ball.dy < 0:
                 ball.dy = -0.2
             ball.dx *= -1
-        # collision fast
+        # speed 2
         if paddle_2.ycor() + 40 > ball.ycor() > paddle_2.ycor() + 10 \
                 or paddle_2.ycor() - 10 > ball.ycor() > paddle_2.ycor() - 40:
             if ball.dy > 0:
@@ -208,7 +211,7 @@ while True:
             if ball.dy < 0:
                 ball.dy = -0.4
             ball.dx = -0.4
-        # collision super fast
+        # speed 3
         if paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() + 40 \
                 or paddle_2.ycor() - 40 > ball.ycor() > paddle_2.ycor() - 50:
             if ball.dy > 0:
@@ -217,6 +220,8 @@ while True:
                 ball.dy = -0.7
             ball.dx = -0.6
         winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    # winner
     if score_1 >= score_win:
         screen.clear()
         screen.bgcolor("black")
@@ -229,11 +234,11 @@ while True:
         time.sleep(3)
         break
 
-    if score_2 >= score_win:
+    elif score_2 >= score_win:
+        hud.color('yellow')
         screen.clear()
         screen.bgcolor("black")
         hud.goto(0, 0)
-        hud.color('yellow')
         hud.write(
             str(player_2) + " won the match! ", align="center",
             font=("Press Start 2P", 32, "normal")
